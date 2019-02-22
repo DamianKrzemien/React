c090e8af97cd4545eda6d7b333ca505a3a2da107
Damian <damian.krzem@gmail.com>
2019-02-22 19:07:50 +0100

    lesson 1

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,28 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
-  render() {
+  state = {    
+  persons: [
+    { name: 'Damian', age: 26},
+    { name: 'Manu', age: 29},
+    { name: 'Stephanie', age: 28}
+    ],
+    otherState: 'some value'
+  }
+
+   switchNameHandler = (newname) => {
+    //console.log('was clcked');
+    this.setState({ 
+     persons: [
+      { name: newname, age: 26},
+      { name: 'Manu', age: 29},
+      { name: 'Stephanie', age: 27}
+     ] 
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({ 
+      persons: [
+       { name: 'Damian', age: 26},
+       { name: event.target.value, age: 29},
+       { name: 'Stephanie', age: 27}
+      ] 
+     })
+  }
+
+  render () {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1p solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi i am React app</h1>
+        <p>This is really working</p>
+        <button 
+          style ={style}
+          onClick={() => this.switchNameHandler('Damian Krzemień!!!')}>Switch Name</button>
+        <Person  
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} 
+          click={this.switchNameHandler.bind(this, "Damian Krzemień!!!")} >My hobby is something</Person>
+        <Person  
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          changed={this.nameChangedHandler}/>
+        <Person  
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
       </div>
     );
+    //return React.createElement('div', {className: 'App'}, React.createElement('H1', null, 'Does this work now?'))
   }
 }
 
 export default App;
+
